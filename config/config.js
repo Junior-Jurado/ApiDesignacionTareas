b888722940620020575e77a7817593422f538c83
Germán Jurado <juniorjurado2004@gmail.com>
2025-09-19 18:16:20 -0500

    Se realizaron modificaciones a la forma de conexion de la base de datos

--- config/config.js
+++ config/config.js
@@ -1,40 +1,40 @@
-const promise = require("bluebird")
+const promise = require("bluebird");
 const options = {
-    promiseLib: promise,
-    query: (e) =>{}
-}
-const https = require('https');
+  promiseLib: promise,
+  query: (e) => {},
+};
 const AWS = require("aws-sdk");
+const pgp = require("pg-promise")(options);
 
-const pgp = require('pg-promise')(options);
 const types = pgp.pg.types;
 types.setTypeParser(1114, (stringValue) => stringValue);
 
-// Configuración de Secrets Manager
-const secretsManager = new AWS.SecretsManager({ region: process.env.AWS_REGION || "us-east-1" });
+const secretsManager = new AWS.SecretsManager({
+  region: process.env.AWS_REGION || "us-east-1",
+});
+
 
 async function getDbConfig() {
-	const secret = await secretsManager
-		.getSecretValue({ SecretId: "db_designacion_tareas_credentials" })
-		.promise();
-	
-	const creds = JSON.parse(secret.SecretString);
+  const secret = await secretsManager
+    .getSecretValue({ SecretId: "db_designacion_tareas_credentials" })
+    .promise();
 
-	return {
-		host: creds.host,
-		port: creds.port || 5432,
-		database: creds.dbname,
-		user: creds.username,
-		password: creds.password,
-		ssl: { rejectUnauthorized: false } 
-	};
+  const creds = JSON.parse(secret.SecretString);
+
+  return {
+    host: creds.host,
+    port: creds.port || 5432,
+    database: creds.dbname,
+    user: creds.username,
+    password: creds.password,
+    ssl: { rejectUnauthorized: false },
+  };
 }
 
-// Inicializa conexión dinámica
+
 async function initDb() {
-	const databaseConfig = await getDbConfig();
-	const db = await getDbConfig();
-	return db;
+  const databaseConfig = await getDbConfig();
+  return pgp(databaseConfig); 
 }
 
 module.exports = initDb;
